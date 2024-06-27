d4cd3ee5d752554ee2f46d3b258f522445fafc46
Ashique <ashiqurrahman2092003@gmail.com>
2024-06-27 21:42:37 +0600

    mod-loader-done

--- src/Components/AllArt/AllArt.jsx
+++ src/Components/AllArt/AllArt.jsx
@@ -2,7 +2,8 @@ import { useLoaderData } from "react-router-dom"
 import AllCard from "./AllCard"
 
 const AllArt = ()=>{
-    
+    const arts = useLoaderData()
+    console.log(arts)
     return(
         <div className="p-2">
             <div>

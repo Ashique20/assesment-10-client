f2c787def152980f439837f1d3cb5cbc3476dd28
Ashique <ashiqurrahman2092003@gmail.com>
2024-06-27 21:47:42 +0600

    mod-nav-context-done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ import { authContext } from "./AuthProvider/AuthProvider"
 import { getAuth, signOut } from "firebase/auth"
 
 const Navbar = () => {
+    const { user,setLoading } = useContext(authContext)
     const logOut = () => {
         const auth = getAuth();
         setLoading(true)
